7c323cf52d1c44556e6989010ffa28717c6506d4
Prystauka Yauheni <34281826+Zheka1389@users.noreply.github.com>
2019-02-17 01:15:55 +0300

    1

--- NullableTypeExtensions.cs
+++ NullableTypeExtensions.cs
@@ -6,8 +6,10 @@ namespace PadawansTask10
     {
         public static bool IsNull(this object variable)
         {
-            // put your code here
-            throw new NotImplementedException();
+            if (variable == null)
+                return true;
+            else
+                return false;
         }
     }
 }
